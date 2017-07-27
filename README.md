8c7d1e65f17425388d1e7e409aac08dae0a29fab
Eric Abell <ericabell@users.noreply.github.com>
2017-07-27 12:59:49 -0500

    First three questions complete

---
+++ README.md
@@ -0,0 +1,1 @@
+# Read through a dataset from etsy.com and solve the following six problems.
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Since the `data.js` file is loaded in your `index.html` before this one, 
+// Since the `data.js` file is loaded in your `index.html` before this one,
 // you have access to the `data` variable that contains the Etsy Store data.
 // Open your `index.html` page in your browser and you can verify that the following
 // is properly working. The `data` variable is an array with 25 items in it
@@ -9,6 +9,11 @@ console.log(data);
 // 1: Show me how to calculate the average price of all items.
 function question1 () {
   // Answer:
+  let total = 0;
+  data.forEach(function(e) {
+    total += e.price;
+  });
+  console.log( total/data.length );
 }
 
 
@@ -16,6 +21,13 @@ function question1 () {
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
 function question2 () {
   // Answer:
+  let itemsSelected = [];
+  data.forEach(function(e) {
+    if( e.price >= 14.00 && e.price <= 18.00 ) {
+      itemsSelected.push(e);
+    }
+  });
+  console.log(itemsSelected);
 }
 
 
@@ -23,6 +35,16 @@ function question2 () {
 // 3: Which item has a "GBP" currency code? Display it's name and price.
 function question3 () {
   // Answer:
+  let itemsSelected = [];
+  data.forEach(function(e) {
+    if( e.currency_code === 'GBP' ) {
+      itemsSelected.push(e);
+    }
+  });
+  itemsSelected.forEach(function(e) {
+    console.log("Name: " + e.title);
+    console.log("Price: " + e.price);
+  });
 }
 
 
@@ -32,7 +54,7 @@ function question4 () {
 }
 
 
-// 5: Which items are made of eight or more materials? 
+// 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
 function question5 () {
   // Answer:
